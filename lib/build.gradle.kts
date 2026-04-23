b2533dc70860c7bebedb1b3c43abf592d7c8df6a
limuyang <limuyang2@hotmail.com>
2026-04-23 14:57:49 +0800

    chore(build): 配置库模块的maven发布与签名
    - 调整minSdk版本为21以支持更低版本设备
    - 引入maven-publish和signing插件用于发布与签名
    - 添加从local.properties文件加载签名相关属性的逻辑
    - 配置发布信息包括groupId、artifactId、version及POM元数据
    - 配置发布仓库路径指向项目根目录下的RepoDir文件夹
    - 根据任务图自动设置项目的签名属性
    - 实现发布组件的签名功能
    - 将RepoDir添加到.gitignore文件避免提交发布库文件目录

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,5 +1,11 @@
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
+    `maven-publish`
+    signing
 }
 
 android {
@@ -11,7 +17,7 @@ android {
     }
 
     defaultConfig {
-        minSdk = 23
+        minSdk = 21
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -47,4 +53,99 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+
+//---------- maven upload info -----------------------------------
+
+val versionName = "1.0.0"
+
+var signingKeyId = ""//签名的密钥后8位
+var signingPassword = ""//签名设置的密码
+var secretKeyRingFile = ""//生成的secring.gpg文件目录
+
+
+val localProperties: File = project.rootProject.file("local.properties")
+
+if (localProperties.exists()) {
+    println("Found secret props file, loading props")
+    val properties = Properties()
+
+    InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
+        properties.load(reader)
+    }
+    signingKeyId = properties.getProperty("signing.keyId")
+    signingPassword = properties.getProperty("signing.password")
+    secretKeyRingFile = properties.getProperty("signing.secretKeyRingFile")
+
+} else {
+    println("No props file, loading env vars")
+}
+
+afterEvaluate {
+
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components.findByName("release"))
+                groupId = "io.github.limuyang2"
+                artifactId = "xxhash"
+                version = versionName
+
+                pom {
+                    name.value("xxhash")
+                    description.value("A xxhash Android library.")
+                    url.value("https://github.com/limuyang2/fast-xxhash-android")
+
+                    licenses {
+                        license {
+                            //协议类型
+                            name.value("The MIT License")
+                            url.value("https://github.com/limuyang2/fast-xxhash-android/blob/main/LICENSE")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.value("limuyang2")
+                            name.value("limuyang")
+                            email.value("limuyang2@hotmail.com")
+                        }
+                    }
+
+                    scm {
+                        connection.value("scm:git@github.com:limuyang2/fast-xxhash-android.git")
+                        developerConnection.value("scm:git@github.com:limuyang2/fast-xxhash-android.git")
+                        url.value("https://github.com/limuyang2/fast-xxhash-android")
+                    }
+                }
+            }
+
+        }
+
+        repositories {
+            maven {
+                setUrl("$rootDir/RepoDir")
+            }
+        }
+
+
+
+    }
+
+}
+
+gradle.taskGraph.whenReady {
+    if (allTasks.any { it is Sign }) {
+
+        allprojects {
+            extra["signing.keyId"] = signingKeyId
+            extra["signing.secretKeyRingFile"] = secretKeyRingFile
+            extra["signing.password"] = signingPassword
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
